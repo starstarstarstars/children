b5c1e02d5eba5604ed8622ec20ac073f63a2388b
starstarstarstars <45023279+starstarstarstars@users.noreply.github.com>
2018-11-18 12:04:14 +0800

    Create list_tuple_map.py

---
+++ app1/list_tuple_map.py
@@ -0,0 +1,12 @@
+wizard_list = 'spider legs toe of frog ,eye of newt ,bat wing slug butter,snake dandruff'
+print(wizard_list)
+wizard_list = ['spider legs',' toe of frog ','eye of newt' ,'bat wing',' slug butter','snake dandruff']
+print(wizard_list)
+wizard_list.append('bear burp')
+print(wizard_list)
+list1 = [1,2,3,4]
+list2 = ['I ','ate','chocolate','and','I','want','more']
+list3 = [list1, list2]
+list4 = list1 +list2
+print('list3',list3)
+print('list4',list4)
