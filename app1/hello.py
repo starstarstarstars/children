cb2a7b714a036a28dc5c7838467a1af809cf2f22
starstarstarstars <45023279+starstarstarstars@users.noreply.github.com>
2018-11-18 12:02:29 +0800

    Update hello.py

--- app1/hello.py
+++ app1/hello.py
@@ -1,3 +1,18 @@
 print("Hello World!")
 A = 5+ 30*20
 print(A)
+fred = "why do grillas have big nostrils? Big Fingers!!"
+print(fred)
+fred = 'why do grillas have big nostrils? Big Fingers!!'
+print(fred)
+fred = '''How do dinosaurs pay their bills?
+with tyrannosaurus!'''
+print(fred)
+silly_string = '''He said,"Aren't can't shouldn't wouldn't."'''
+print(silly_string)
+silly_string = '''He said,\\"Aren't can't shouldn't wouldn't."'''
+print(silly_string)
+myscore = 1000
+message = 'I scored %s points'
+print(message % myscore)
+
