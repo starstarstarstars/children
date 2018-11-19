9aa6f2172c4356c28a2a14824a77a76215e23867
starstarstarstars <45023279+starstarstarstars@users.noreply.github.com>
2018-11-19 16:46:06 +0800

    reuse

--- app1/cycle.py
+++ app1/cycle.py
@@ -1,3 +1,13 @@
 for x in range(0,5):
     print('hello')
 print(list(range(10,20)))
+hugehairypants = ['huge','hairy','pants']
+for i in hugehairypants:
+    print(i)
+    print(i)
+print('''???????????block??????????''')
+hugehairypants = ['huge','hairy','pants']
+for i in hugehairypants:
+    print(i)
+    for j in hugehairypants:
+        print(j)
