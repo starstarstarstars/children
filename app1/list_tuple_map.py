9251e07bd6f1258c28395624db877b6539397a32
starstarstarstars <45023279+starstarstarstars@users.noreply.github.com>
2018-11-19 09:13:43 +0800

    Update list_tuple_map.py

--- app1/list_tuple_map.py
+++ app1/list_tuple_map.py
@@ -10,3 +10,8 @@ list3 = [list1, list2]
 list4 = list1 +list2
 print('list3',list3)
 print('list4',list4)
+favorite_sports = {'ralph williams':'football','michael tippett':'basketball','edward elgar':'baseball','rebecca clarke':'netball','ethel smyth':'badminton','frank brigge':'rugby'}
+#print('use of dictionary ',favorit_sports['rebecca clarke'])
+print(favorite_sports['rebecca clarke'])
+
+
