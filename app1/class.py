ec2d7af787535871ca8c28e077c7c921e0c1714a
starstarstarstars <45023279+starstarstarstars@users.noreply.github.com>
2018-11-19 19:28:20 +0800

    class

---
+++ app1/class.py
@@ -0,0 +1,14 @@
+class Things:
+    pass
+class Inanimate(Things):
+    pass
+class Animate(Things):
+    pass
+class Sidewalks(Inanimate):
+    pass
+class Animals(Animate):
+    pass
+class Mammals(Animals):
+    pass
+class Giraffes(Mammals):
+    pass
