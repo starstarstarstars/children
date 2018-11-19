83a9bc28ce20e158a80005b8676aed2a6f0b3bcf
starstarstarstars <45023279+starstarstarstars@users.noreply.github.com>
2018-11-19 10:26:50 +0800

    Update ifelse.py

---
+++ app1/ifelse.py
@@ -0,0 +1,27 @@
+age = 13
+if age > 20:
+    print('you are too old')
+if age > 12:
+    print('you are too old')
+    print('why are you here?')
+    print('why aren\'t you mowing a lawn or sorting papers?')
+if age > 21:
+    print('you are too young')
+    print('why are you here?')
+print('why aren\'t you mowing a lawn or sorting papers?')
+    
+age =12
+if age == 10:
+    print("what do you call an unhappy cranberry")
+    print('a blueberry')
+elif 11 == age:
+    print('what did the green graph say to the blue grape')
+    print('breathe! breathe!')
+elif 12 == age:
+    print("what did you 0 say to 8")
+    print('Hi guys')
+elif 13 == age:
+    print("why was't 10 afraid of 7")
+    print("because rather than eating 9,7 8 pi.")
+else:
+    print("Huh?")
